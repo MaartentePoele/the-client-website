d462d228a4fe4c50c593d792734bb3f37d45e80b
MaartentePoele <maarten.te.poele@hva.nl>
2025-10-06 16:19:40 +0200

    #18 Header met animatie aangepast als je scrollt

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-// JS
+let prevScrollPos = window.pageYOffset;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > prevScrollPos) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
+  }
+  prevScrollPos = currentScrollPos;
+});
